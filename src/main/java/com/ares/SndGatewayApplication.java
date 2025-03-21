872deaffdf593fc90c2948d498c108501221c8c8
ares08 <ares08@ares08.local>
2025-03-21 20:49:28 +0800

    [ADD]init project

---
+++ src/main/java/com/ares/SndGatewayApplication.java
@@ -0,0 +1,22 @@
+package com.ares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SndGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SndGatewayApplication.class, args);
+	}
+
+	@GetMapping("/health")
+	public String hello() {
+		return "Hello World!";
+	}
+}
---
+++ src/test/java/com/ares/SdnGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdnGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
